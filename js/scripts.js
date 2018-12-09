8d614628a52fd8d0c3787d26c3df9b36952b113e
elham-malekmohamadi <elhammmalekmohamadi@gmail.com>
2018-12-09 13:21:19 +0330

    js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 $(".dropdown-list").click(function () {
   $(".mainDropDownMenu").toggle();
 });
+
 // mobilemenu
 $(".iconbar").click(function(){
   $(".mainmenuMobile").toggle();
+  if ($('.mainDropDownMenuMobile').css('display') == 'block') {
+    $('.mainDropDownMenuMobile').css({'display':'none'});
+  }
 });
 $(".dropdown-list-mobile").click(function () {
   $(".mainDropDownMenuMobile").toggle();
